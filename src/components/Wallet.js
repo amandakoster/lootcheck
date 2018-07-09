c93ffdc9c605d0890fd3c6eb5bd961cb01695f75
Amanda Koster <amandakoster@gmail.com>
2018-07-09 11:39:54 -0700

    wallet balance test pasts testing a string value wth enzyme

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -6,12 +6,11 @@ export class Wallet extends Component{
   render() {
     return(
       <div>
-        <h3>Wallet Balance: {this.props.balance}</h3>
+        <h3 className="balance">Wallet balance: {this.props.balance}</h3>
       </div>
     );
   }
-}
-
+};
 
 
 export default connect(state => { balance: state }, null)(Wallet);
--- src/components/wallet.test.js
+++ src/components/wallet.test.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 //import and test only the exported not-conencted Wallet the test without redux
 import { Wallet}  from './Wallet';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-
 
 
 describe('Wallet', () => {
-  const wallet = shallow(<Wallet />);
+  const props = { balance: 20 };
+  const wallet = shallow(<Wallet {...props}/>);
 
   it('renders properly', () => {
     expect(wallet).toMatchSnapshot();
   });
+
+  it('displays the balance from props', () => {
+    //find balance using .find() + classname of h3 node
+    //use enzyme .text() to examine text of h3 node
+    //.text() will render a string of `balance`
+    expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
+
+  });
 });
 
+  
