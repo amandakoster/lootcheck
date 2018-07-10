20dd3704a1a43a1b79efe12287194fb22c2823ff
Amanda Koster <amandakoster@gmail.com>
2018-07-09 20:35:34 -0700

    building a fetchMock

--- src/actions/bitcoin.test.js
+++ src/actions/bitcoin.test.js
@@ -1,1 +1,11 @@
 // https://api.coindesk.com/v1/bpi/currentprice.json
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-think';
+import fetchMock from 'fetch';
+import { FETCH_BITCOIN } from './constants';
+import { fetchBitcoin } from './bitcoin';
+
+//configureMockStore allows middleware, add middleware thunk
+const createMockStore = configureMockStore([thunk]);
+const store = createMockStore({ bitcoin: {} })
+
